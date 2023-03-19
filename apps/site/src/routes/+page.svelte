51fa6dbf059f7ab602061b5bd42992787e9c1839
Alexander Liu <a@alexanderliu.com>
2023-03-19 11:47:46 -0700

    feat(site): placeholder home page

--- apps/site/src/routes/+page.svelte
+++ apps/site/src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import cover from './cover.jpg';
+</script>
+
+<h1>
+	We’re Cyber @ UCI, the club for <br />all things cybersecurity. <br />
+	We host weekly beginner friendly <br /> workshops, competitions, and challenges.
+</h1>
+<img src={cover} />
+
+<style lang="scss">
+	h1 {
+		font-size: 2rem;
+		font-weight: 400;
+		line-height: 130%;
+		max-width: 35ch;
+		padding: 0 16px;
+		margin: 96px auto;
+		text-align: center;
+	}
+	img {
+		width: calc(100% - 24px * 2);
+		margin: 0 24px;
+		height: 512px;
+		object-fit: cover;
+		object-position: 50% 40%;
+		border-radius: 16px;
+	}
+</style>
