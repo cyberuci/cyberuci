2c190234f10a5a45683d72483781d59de6366a1e
Alexander Liu <a@alexanderliu.com>
2023-03-19 08:55:19 -0700

    feat(site): events page

--- apps/site/src/routes/events/+page.svelte
+++ apps/site/src/routes/events/+page.svelte
@@ -9,6 +9,57 @@
 	<title>Events — Cyber @ UCI</title>
 </svelte:head>
 
-{#each data.events as event}
-	<Event {event} />
-{/each}
+<main>
+	<h1>Events</h1>
+	<div class="line" />
+	<div class="grid">
+		<div class="events">
+			{#each data.events as event}
+				<Event {event} />
+			{/each}
+		</div>
+	</div>
+</main>
+
+<style lang="scss">
+	main {
+		display: grid;
+		grid-template-columns: repeat(5, 1fr);
+		gap: 0 24px;
+	}
+
+	h1 {
+		grid-column: 2 / span 3;
+		grid-row: 1;
+		margin: 96px 0 8px 0;
+		font-family: 'Inter';
+		font-size: 40px;
+		letter-spacing: -0.022em;
+		line-height: 56px;
+		font-weight: 400;
+	}
+
+	.line {
+		grid-column: 1 / span 5;
+		grid-row: 2;
+		width: 100%;
+		height: 1px;
+		background-color: var(--gray7);
+	}
+
+	.grid {
+		margin: 24px 0;
+		grid-column: 2 / span 3;
+		grid-row: 3;
+		display: flex;
+		flex-direction: column;
+		gap: 16px;
+	}
+
+	.events {
+		grid-column: 2 / span 4;
+		display: flex;
+		flex-direction: column;
+		gap: 16px;
+	}
+</style>
--- apps/site/src/routes/events/[slug]/+page.svelte
+++ apps/site/src/routes/events/[slug]/+page.svelte
@@ -27,7 +27,70 @@
 	<title>{title} — Cyber @ UCI</title>
 </svelte:head>
 
-<h1>{title}</h1>
-<span>{startLocaleString} - {endLocaleString}</span>
-<span>{location}</span>
-<PortableText value={content} />
+<main>
+	<div class="column-1">
+		<a href="/events">Back</a>
+		<div class="time">
+			<p>{startLocaleString}</p>
+			<p>{endLocaleString}</p>
+		</div>
+		<span>{location}</span>
+	</div>
+
+	<h1>{title}</h1>
+	<div class="content">
+		{#if content}
+			<PortableText value={content} />
+		{/if}
+	</div>
+</main>
+
+<style lang="scss">
+	main {
+		display: grid;
+		grid-template-columns: repeat(5, 1fr);
+		gap: 16px;
+		margin: 0 48px;
+	}
+
+	.column-1 {
+		grid-column: 1;
+		grid-row: 2;
+		display: flex;
+		flex-direction: column;
+		gap: 24px;
+		color: var(--gray11);
+
+		.time p {
+			margin: 6px 0;
+		}
+
+		a {
+			line-height: 27px;
+		}
+
+		span {
+			line-height: 27px;
+		}
+	}
+
+	.content {
+		grid-column: 2 / span 3;
+		grid-row: 2;
+	}
+
+	h1 {
+		grid-column: 2 / span 3;
+		grid-row: 1;
+		font-family: 'Inter';
+		margin: 96px 0 24px 0;
+		font-size: 40px;
+		letter-spacing: -0.022em;
+		line-height: 56px;
+		font-weight: 400;
+	}
+
+	main {
+		grid-column: 2 / span 3;
+	}
+</style>
