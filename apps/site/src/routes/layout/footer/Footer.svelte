8e9a6f38139a636bc200fe82cd826e3c0cf153a3
Alexander Liu <a@alexanderliu.com>
2023-04-10 20:07:16 -0700

    feat: footer parallax animation

--- apps/site/src/routes/layout/footer/Footer.svelte
+++ apps/site/src/routes/layout/footer/Footer.svelte
@@ -1,33 +1,74 @@
-<div class="cyber">
-	<p>Cyber @ UCI&#160Cyber @ UCI&#160</p>
+<script lang="ts">
+	import { debounce } from 'debounce';
+
+	let wrapper: HTMLDivElement;
+	let innerHeight: number;
+	let top: number;
+	$: offset = (innerHeight - top) * 0.25;
+
+	// TODO: Only calculate when footer is in view.
+	const calculateTop = debounce(() => {
+		top = wrapper.getBoundingClientRect().top;
+	}, 5);
+</script>
+
+<svelte:window bind:innerHeight on:resize={calculateTop} on:scroll={calculateTop} />
+
+<div id="wrapper">
+	<div id="transform" style:transform="translateY({offset}px)">
+		<div class="cyber">
+			<p>Cyber @ UCI&#160Cyber @ UCI&#160</p>
+		</div>
+		<footer>
+			<p class="copyright">Copyright © 2023 Cyber @ UCI</p>
+			<nav>
+				<a href="https://discord.gg/WjbSP2B9Ck">Discord</a>
+				<a href="https://instagram.com/cyberuci/">Instagram</a>
+				<a
+					href="https://uci.us18.list-manage.com/subscribe?u=0029dcacaf8ca6a3b88c61750&id=701d18798e"
+				>
+					Mailing List
+				</a>
+			</nav>
+			<nav>
+				<a href="/brand">Brand</a>
+				<a href="/contact">Contact</a>
+				<a href="/acknowledgments">Acknowledgments</a>
+			</nav>
+			<nav>
+				<a href="mailto:cyberclub@uci.edu">cyberclub@uci.edu</a>
+			</nav>
+		</footer>
+	</div>
+	<div bind:this={wrapper} />
 </div>
-<footer>
-	<p class="copyright">Copyright © 2023 Cyber @ UCI</p>
-	<nav>
-		<a href="https://discord.gg/WjbSP2B9Ck">Discord</a>
-		<a href="https://instagram.com/cyberuci/">Instagram</a>
-		<a href="https://uci.us18.list-manage.com/subscribe?u=0029dcacaf8ca6a3b88c61750&id=701d18798e">
-			Mailing List
-		</a>
-	</nav>
-	<nav>
-		<a href="/brand">Brand</a>
-		<a href="/contact">Contact</a>
-		<a href="/acknowledgments">Acknowledgments</a>
-	</nav>
-	<nav>
-		<a href="mailto:cyberclub@uci.edu">cyberclub@uci.edu</a>
-	</nav>
-</footer>
 
 <style lang="scss">
+	#wrapper {
+		background: hsla(218, 96%, 90%, 1);
+		overflow: hidden;
+		margin: 48px 0 0 0;
+
+		#transform {
+			transition: transform 5ms;
+		}
+	}
+
 	.cyber {
 		overflow: hidden;
 		padding: 48px 0 0 0;
-		margin: 48px 0 0 0;
-		background: hsla(218, 96%, 90%, 1);
 
 		p {
+			@keyframes scroll {
+				from {
+					transform: translateX(0);
+				}
+
+				to {
+					transform: translateX(-50%);
+				}
+			}
+
 			animation: scroll 15s linear infinite;
 			width: max-content;
 			color: #a9cafc;
@@ -42,19 +83,8 @@
 		}
 	}
 
-	@keyframes scroll {
-		from {
-			transform: translateX(0);
-		}
-
-		to {
-			transform: translateX(-50%);
-		}
-	}
-
 	footer {
-		padding: 16px 24px;
-		background: hsla(218, 96%, 90%, 1);
+		padding: 16px 24px 64px 24px;
 		color: #000000;
 		display: grid;
 		gap: 16px;
@@ -65,7 +95,6 @@
 		}
 
 		.copyright {
-			font-size: 0.875rem;
 			margin: 0;
 			grid-column: 1;
 			@media (min-width: 960px) {
