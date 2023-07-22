7f19946828daa9619af6b4410e0b5602603f9fcf
Alexander Liu <a@alexanderliu.com>
2023-07-21 22:35:19 -0400

    feat: switch to vercel analytics

--- apps/site/src/routes/+layout.svelte
+++ apps/site/src/routes/+layout.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import './global.scss';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
 	import Header from './layout/header/Header.svelte';
 	import Footer from './layout/footer/Footer.svelte';
+
+	inject({ mode: dev ? 'development' : 'production' });
 
 	export let data: PageData;
 </script>
 
 <svelte:head>
-	<script src="https://cdn.usefathom.com/script.js" data-site="OYOSQPKX" defer></script>
 	<link href="/fonts/blinker/blinker.css" rel="stylesheet" />
 	<link href="/fonts/jura/jura.css" rel="stylesheet" />
 	<link href="/fonts/tiny/TINY.css" rel="stylesheet" />
