54e3ae20d3261fbb103678f63c8335b43aa8f10c
Alexander Liu <a@alexanderliu.com>
2024-01-15 22:01:23 -0800

    feat: homepage header section

--- apps/site/src/routes/(homepage)/+page.svelte
+++ apps/site/src/routes/(homepage)/+page.svelte
@@ -1,33 +1,78 @@
-<script>
-	import cover from './cover.jpg';
+<script lang="ts">
+	import CTAButton from './CTAButton.svelte';
+	import { ActivitySquare, CalendarDays, Medal } from 'lucide-svelte';
+	import { siDiscord } from 'simple-icons';
 </script>
 
 <svelte:head>
 	<title>Cyber @ UCI</title>
 </svelte:head>
 
-<h1>
-	We’re Cyber @ UCI, the club for <br />all things cybersecurity. <br />
-	We host weekly beginner friendly <br /> workshops, competitions, and challenges.
-</h1>
-<img src={cover} />
+<div class="header">
+	<h1 class="heading">
+		Cybersecurity matters.<br />
+		Be part of the solution.
+	</h1>
+	<p class="description">
+		Cyber@UCI is a community dedicated to making cybersecurity accessible to everyone, regardless of
+		experience.
+	</p>
+	<div class="cta-buttons">
+		<CTAButton href="/hackerlab" icon={ActivitySquare} label="Visit HackerLab" />
+		<CTAButton href="/events" icon={CalendarDays} label="Visit Events" />
+		<CTAButton href="/discord" svgPath={siDiscord.path} label="Join Our Discord" />
+		<CTAButton href="/competition" icon={Medal} label="Competition Teams" />
+	</div>
+	<enhanced:img class="background-image" src="./cover.jpg" alt="" />
+</div>
 
 <style lang="scss">
-	h1 {
-		font-size: 2rem;
+	.header {
+		position: relative;
+		border-radius: 48px;
+		overflow: hidden;
+		margin: 16px;
+		padding: calc(96px + 40px) 24px;
+	}
+
+	.heading {
+		font-feature-settings: 'dlig' on;
+		font-family: 'BBB Strobo';
+		font-stretch: 175%;
+		font-size: 48px;
 		font-weight: 400;
 		line-height: 130%;
-		max-width: 35ch;
-		padding: 0 16px;
-		margin: 96px auto;
-		text-align: center;
+		margin: 0 0 32px 0;
 	}
-	img {
-		width: calc(100% - 24px * 2);
-		margin: 0 24px;
-		height: 512px;
+
+	.description {
+		margin: 32px 0 48px 0;
+		max-width: 672px;
+		text-wrap: balance;
+		font-feature-settings: 'ss05' on;
+		font-family: CommitMonoV142;
+		font-size: 16px;
+		font-weight: 400;
+		line-height: 150%;
+	}
+
+	.cta-buttons {
+		width: max-content;
+		display: grid;
+		gap: 1px;
+		grid-template-columns: 1fr 1fr;
+		border-radius: 6px;
+		overflow: hidden;
+	}
+
+	.background-image {
+		position: absolute;
+		z-index: -1;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 100%;
 		object-fit: cover;
-		object-position: 50% 40%;
-		border-radius: 16px;
+		filter: brightness(0.13);
 	}
 </style>
---
+++ apps/site/src/routes/(homepage)/CTAButton.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import type { ComponentType } from 'svelte';
+
+	export let href: string;
+	export let icon: ComponentType | undefined = undefined;
+	export let svgPath: string | undefined = undefined;
+	export let label: string;
+</script>
+
+<a class="cta-button" {href}>
+	{#if icon}
+		<svelte:component this={icon} size={16} />
+	{:else if svgPath}
+		<svg
+			width={16}
+			height={16}
+			role="img"
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="currentColor"
+		>
+			<path d={svgPath} />
+		</svg>
+	{:else}
+		<div />
+	{/if}
+
+	<span class="label">{label}</span>
+</a>
+
+<style lang="scss">
+	.cta-button {
+		all: unset;
+		cursor: pointer;
+		width: 192px;
+		height: 96px;
+		padding: 12px;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		border-radius: 2px;
+		color: #b4b4b4;
+		background-color: rgba(255, 255, 255, 0.07);
+		transition:
+			color 150ms ease-out,
+			background-color 150ms ease-out;
+
+		&:hover {
+			color: #202020;
+			background-color: rgba(255, 255, 255, 0.93);
+		}
+	}
+
+	.label {
+		font-feature-settings: 'ss05' on;
+		font-family: CommitMonoV142;
+		font-size: 14px;
+		font-weight: 400;
+		line-height: 1;
+	}
+</style>
