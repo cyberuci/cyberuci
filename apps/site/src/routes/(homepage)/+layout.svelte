06de0053886de92dff88af1ce2b5a8b6be00b7b5
Alexander Liu <a@alexanderliu.com>
2024-01-15 22:01:23 -0800

    feat: homepage header section

--- apps/site/src/routes/(homepage)/+layout.svelte
+++ apps/site/src/routes/(homepage)/+layout.svelte
@@ -10,9 +10,7 @@
 </script>
 
 <svelte:head>
-	<link href="/fonts/blinker/blinker.css" rel="stylesheet" />
-	<link href="/fonts/jura/jura.css" rel="stylesheet" />
-	<link href="/fonts/tiny/TINY.css" rel="stylesheet" />
+	<link href="/fonts/strobo/strobo.css" rel="stylesheet" />
 </svelte:head>
 
 <main>
