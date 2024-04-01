d893135eb9673c2564710e2ef1fbbc45035b32d3
Alexander Liu <a@alexanderliu.com>
2024-04-01 05:47:02 -0700

    feat: homepage nav

--- apps/site/src/routes/(homepage)/+page.svelte
+++ apps/site/src/routes/(homepage)/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 
+	import { default as Nav } from '../(legacy)/layout/header/Header.svelte';
 	import Header from './sections/Header/Header.svelte';
 	import Events from './sections/Events/Events.svelte';
 	import Competitions from './sections/Competitions/Competitions.svelte';
 	import Hackerlab from './sections/Hackerlab/Hackerlab.svelte';
 	import Socials from './sections/Socials/Socials.svelte';
-	import Footer from '../(legacy)/layout/footer/Footer.svelte';
 
 	export let data: PageData;
 </script>
@@ -15,6 +15,7 @@
 	<title>Cyber @ UCI</title>
 </svelte:head>
 
+<Nav />
 <Header />
 <Events events={data.events} />
 <Competitions
