efa14bc2863ebf625d128aafd8f9cff6c3b066e9
Alexander Liu <a@alexanderliu.com>
2024-12-30 23:20:23 -0500

    feat: nav

---
+++ apps/site/src/lib/common/components/Nav.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Logo from './Logo.svelte';
+</script>
+
+<div class="flex my-3 mx-default-x">
+	<div class="size-8"><Logo /></div>
+</div>
--- apps/site/uno.config.ts
+++ apps/site/uno.config.ts
@@ -23,50 +23,57 @@ export default defineConfig({
 		'background-2': 'bg-gray-2 dark:bg-graydark-2',
 		text: 'text-gray-12 dark:text-graydark-12'
 	},
-	extendTheme: (theme) => ({
-		...theme,
-		fontFamily: {
-			mono: 'CommitMono, ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
-			sans: 'Blinker, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
-			strobo: '"BBB Strobo", sans-serif',
-			explorer: 'TASAExplorer, sans-serif'
-		},
-		colors: {
-			gray: Object.fromEntries(Object.values(gray).map((color, i) => [i + 1, color])),
-			graydark: Object.fromEntries(Object.values(grayDark).map((color, i) => [i + 1, color])),
+	extendTheme: (theme) => {
+		console.log(theme);
 
-			// cyber accent color
-			// https://www.radix-ui.com/colors/custom
-			// accent: #2975BA, background: #FBFDFE
-			blue: {
-				1: '#f7fbff',
-				2: '#f0f6fe',
-				3: '#e1f1ff',
-				4: '#d2eaff',
-				5: '#c0dfff',
-				6: '#acd2fa',
-				7: '#91c1f2',
-				8: '#68a9ea',
-				9: '#2975ba',
-				10: '#1767ab',
-				11: '#1b6aae',
-				12: '#1b3d5d'
+		return {
+			...theme,
+			fontFamily: {
+				mono: 'CommitMono, ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+				sans: 'Blinker, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+				strobo: '"BBB Strobo", sans-serif',
+				explorer: 'TASAExplorer, sans-serif'
 			},
-			// accent: #2975BA, background: #081826
-			bluedark: {
-				1: '#0e1822',
-				2: '#101b27',
-				3: '#0b2945',
-				4: '#00345e',
-				5: '#054171',
-				6: '#144f83',
-				7: '#20609a',
-				8: '#2773b8',
-				9: '#2975ba',
-				10: '#2968a2',
-				11: '#75bbff',
-				12: '#cae6ff'
+			colors: {
+				gray: Object.fromEntries(Object.values(gray).map((color, i) => [i + 1, color])),
+				graydark: Object.fromEntries(Object.values(grayDark).map((color, i) => [i + 1, color])),
+
+				// cyber accent color
+				// https://www.radix-ui.com/colors/custom
+				// accent: #2975BA, background: #FBFDFE
+				blue: {
+					1: '#f7fbff',
+					2: '#f0f6fe',
+					3: '#e1f1ff',
+					4: '#d2eaff',
+					5: '#c0dfff',
+					6: '#acd2fa',
+					7: '#91c1f2',
+					8: '#68a9ea',
+					9: '#2975ba',
+					10: '#1767ab',
+					11: '#1b6aae',
+					12: '#1b3d5d'
+				},
+				// accent: #2975BA, background: #081826
+				bluedark: {
+					1: '#0e1822',
+					2: '#101b27',
+					3: '#0b2945',
+					4: '#00345e',
+					5: '#054171',
+					6: '#144f83',
+					7: '#20609a',
+					8: '#2773b8',
+					9: '#2975ba',
+					10: '#2968a2',
+					11: '#75bbff',
+					12: '#cae6ff'
+				}
+			},
+			spacing: {
+				'default-x': '0.75rem'
 			}
-		}
-	})
+		};
+	}
 });
