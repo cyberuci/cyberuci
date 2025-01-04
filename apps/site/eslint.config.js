4f6ef48bc04b1c70997300073b1841a3a5eaae68
Alexander Liu <a@alexanderliu.com>
2025-01-03 19:35:22 -0500

    chore: eslint flat config import order

---
+++ apps/site/eslint.config.js
@@ -0,0 +1,66 @@
+import prettier from 'eslint-config-prettier';
+import js from '@eslint/js';
+import { includeIgnoreFile } from '@eslint/compat';
+import svelte from 'eslint-plugin-svelte';
+import globals from 'globals';
+import eslintPluginImportX from 'eslint-plugin-import-x';
+import { fileURLToPath } from 'node:url';
+import unocss from '@unocss/eslint-config/flat';
+import ts from 'typescript-eslint';
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+
+export default ts.config(
+	includeIgnoreFile(gitignorePath),
+	js.configs.recommended,
+	...ts.configs.recommended,
+	...svelte.configs['flat/recommended'],
+	prettier,
+	...svelte.configs['flat/prettier'],
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node
+			}
+		}
+	},
+	{
+		files: ['**/*.svelte'],
+		languageOptions: {
+			parserOptions: {
+				parser: ts.parser
+			}
+		}
+	},
+	unocss,
+	eslintPluginImportX.flatConfigs.typescript,
+	{
+		settings: {
+			'import-x/order': [
+				'error',
+				{
+					groups: ['type', 'builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+					'newlines-between': 'always',
+					pathGroups: [
+						{
+							pattern: '$app/**',
+							group: 'external',
+							position: 'before'
+						},
+						{
+							pattern: '$env/**',
+							group: 'external',
+							position: 'before'
+						},
+						{
+							pattern: '$lib/**',
+							group: 'internal'
+						}
+					],
+					distinctGroup: true,
+					pathGroupsExcludedImportTypes: ['type']
+				}
+			]
+		}
+	}
+);
