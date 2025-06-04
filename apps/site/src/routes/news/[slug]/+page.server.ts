5d5aa1e2ae7c01ca45d94d1bfc2a011d8b9cfef4
Alexander Liu <a@alexanderliu.com>
2025-06-03 23:33:24 -0700

    fix: news story slug

--- apps/site/src/routes/news/[slug]/+page.server.ts
+++ apps/site/src/routes/news/[slug]/+page.server.ts
@@ -1,15 +1,23 @@
 import type { PageServerLoad } from './$types';
 import { client } from '$lib/sanity/sanityClient';
 import { defineQuery } from 'groq';
+import { error } from '@sveltejs/kit';
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async ({ params: { slug } }) => {
 	const newsStoryPageQuery = defineQuery(`
-    *[_type == "news"][0] {
+   	*[_type == "news" && slug.current == $slug][0] {
 			content,
 			title
 		}
   `);
-	const newsStoryPage = await client.fetch(newsStoryPageQuery);
+
+	const newsStoryPage = await client.fetch(newsStoryPageQuery, {
+		slug
+	});
+
+	if (!newsStoryPage) {
+		return error(404);
+	}
 
 	return { newsStoryPage };
 };
