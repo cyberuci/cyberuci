9c34fc48a4915cf4ae53926903b8182fc4827988
Eric <eriliu@ucdavis.edu>
2025-09-29 01:56:14 -0700

    feat: news covers

--- apps/site/src/routes/news/+page.svelte
+++ apps/site/src/routes/news/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import Title from '$lib/common/components/Title.svelte';
-	import PortableText from '$lib/portableText/PortableText.svelte';
+	import { client } from '$lib/sanity/sanityClient';
+	import imageUrlBuilder from '@sanity/image-url';
 
 	interface Props {
 		data: PageData;
 	}
+
+	const builder = imageUrlBuilder(client);
 
 	let { data }: Props = $props();
 </script>
@@ -16,15 +19,22 @@
 
 <div class="my-40 space-x">
 	<Title title="News" />
-	{#each data.newsPage as { title, slug, date }}
-		<a
-			href="/news/{slug.current}"
-			class="text decoration-none hover:dark:text-bluedark-12 hover:text-blue-12 group"
-		>
-			<span class="block my-5 type-heading-2 max-w-40ch group-hover:decoration-underline"
-				>{title}</span
+	{#each data.newsPage as { title, slug, date, cover }}
+		<div class="group flex gap-5 my-10">
+			<img
+				alt="The cover of the article."
+				class="w-1/2 rounded-sm"
+				src={builder.image(cover).width(1024).height(600).url()}
+				/>
+			<a
+				href="/news/{slug.current}"
+				class="text decoration-none hover:dark:text-bluedark-12 hover:text-blue-12 group"
 			>
-			<span class="block type-label">{date}</span>
-		</a>
+				<span class="block my-5 type-heading-2 max-w-40ch group-hover:decoration-underline"
+					>{title}</span
+				>
+				<span class="block type-label">{date}</span>
+			</a>
+		</div>
 	{/each}
 </div>
