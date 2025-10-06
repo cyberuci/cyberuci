bca7d48de5aa1dd55f612d8bc3f298a8fd21f81d
Eric L <eliu15@uci.edu>
2025-10-06 16:27:34 -0700

    feat: add a simple events page (gcal embed)

---
+++ apps/site/src/routes/events/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Title from '$lib/common/components/Title.svelte';
+</script>
+
+<svelte:head>
+	<title>Events — Cyber @ UCI</title>
+</svelte:head>
+<div class="my-40 space-x">
+	<Title title="Events" />
+	<iframe
+		src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FLos_Angeles&showPrint=0&src=Y3liZXJjbHViQHVjaS5lZHU&src=Y19lbWpxa281OWhmMGFkMWxpM29tdWVrNDBsMEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZWxpdTE1QHVjaS5lZHU&color=%23039be5&color=%23b39ddb&color=%230b8043&color=%23616161"
+		style="border:solid 1px #777"
+		frameborder="0"
+		scrolling="no"
+		class="w-full h-[75vh]"
+		title="Google Calendar"
+	></iframe>
+</div>
