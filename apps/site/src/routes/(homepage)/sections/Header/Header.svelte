0629a27ff3e8aa47854b9e30f3e9c233ba32df7e
Kevin Li <kevinli200555@gmail.com>
2025-11-17 19:37:15 -0800

    - Added a tempoary video file to the \Header folder to act as a placeholder - Changed the margin styling for the "Cybersecruity matters..." Text so that the video and the text fit on one screen - uncommented back the description text - I also changed the .lintstagedrc.json file.  This was because video files would be sent to Prettier, which can't process those. and this was stopping me from committing

--- apps/site/src/routes/(homepage)/sections/Header/Header.svelte
+++ apps/site/src/routes/(homepage)/sections/Header/Header.svelte
@@ -1,11 +1,26 @@
+<script>
+	import videoFile from './RandomTestVideo_1280x720.mp4';
+</script>
+
 <div class="my-6 space-x">
-	<h1 class="type-display mt-60 sm:mt-80">
+	<video
+		width="320"
+		height="240"
+		autoplay
+		loop
+		class="mb-0 block h-auto w-4/5 rounded-lg object-cover shadow-lg"
+	>
+		<source src={videoFile} type="video/mp4" />
+		<track kind="captions" srclang="en" label="English" default />
+		Your browser does not support the video tag.
+	</video>
+	<h1 class="type-display mt-10 sm:mt-10">
 		Cybersecurity matters.<br />
 		Be part of the solution.
 	</h1>
-	<!-- <p class="description">
+	<p class="description">
 		Cyber@UCI is a community dedicated to making cybersecurity accessible to everyone, regardless of
 		experience.
-	</p> -->
+	</p>
 </div>
 <enhanced:img class="h-2xl w-full object-cover" src="./cover.jpg" alt="" />
