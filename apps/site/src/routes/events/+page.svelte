d69936c474ffafa5a0a6070a90bcd0dc2137081f
aanyal <aanyalakhani01@gmail.com>
2026-04-11 22:37:58 -0700

    Added in events from Cyber Google Calendar

--- apps/site/src/routes/events/+page.svelte
+++ apps/site/src/routes/events/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Title from '$lib/common/components/Title.svelte';
+	import type { PageData } from './$types';
 
 	import { onMount } from 'svelte';
 	import { ScheduleXCalendar } from '@schedule-x/svelte';
@@ -9,25 +10,52 @@
 	import { Temporal } from 'temporal-polyfill';
 	import 'temporal-polyfill/global';
 
-	let calendarApp;
+	interface Props {
+		data: PageData;
+	}
+
+	let { data }: Props = $props();
+
+	let calendarApp = $state();
 
 	onMount(() => {
+		const calendar_events = data.events.map((event, i) => {
+			const startStr = event.start.dateTime || `${event.start.date}T00:00:00Z`;
+			const endStr = event.end.dateTime || `${event.end.date}T23:59:59Z`;
+
+			return {
+				id: event.id || `${i}`,
+				title: event.summary,
+				start: Temporal.ZonedDateTime.from(`${startStr}[America/Los_Angeles]`),
+				end: Temporal.ZonedDateTime.from(`${endStr}[America/Los_Angeles]`),
+				calendarId: 'work'
+			};
+		});
+
+		console.log(calendar_events);
+
 		calendarApp = createCalendar({
 			views: [createViewMonthGrid()],
-			events: [
-				{
-					id: '1',
-					title: 'Event 1',
-					start: Temporal.PlainDate.from('2026-04-06'),
-					end: Temporal.PlainDate.from('2026-04-06')
+			calendars: {
+				work: {
+					colorName: 'work',
+					lightColors: {
+						main: '#1a73e8',
+						container: '#d2e3fc',
+						onContainer: '#000000'
+					}
 				},
-				{
-					id: '2',
-					title: 'Event 2',
-					start: Temporal.ZonedDateTime.from('2026-04-06T02:00:00+09:00[Asia/Tokyo]'),
-					end: Temporal.ZonedDateTime.from('2026-04-06T04:00:00+09:00[Asia/Tokyo]')
+				personal: {
+					colorName: 'personal',
+					lightColors: {
+						main: '#00796b',
+						container: '#b9f6ca',
+						onContainer: '#000000'
+					}
 				}
-			]
+			},
+			events: calendar_events,
+			timezone: Intl.DateTimeFormat().resolvedOptions().timeZone
 		});
 	});
 </script>
@@ -62,8 +90,29 @@
 		--sx-color-primary: #888888;
 
 		--sx-color-on-primary-container: #21005e;
-		--sx-color-surface-tint: #ff9db7;
+		--sx-color-surface-tint: #bdbdbd;
 		--sx-color-on-background: #fff;
+	}
+
+	:global(.sx__button:hover) {
+		background-color: #3d3d3d !important;
+	}
+
+	:global(.sx__date-picker__month-view-header__month-year) {
+		font-size: 1rem !important;
+	}
+
+	:global(.sx__calendar .sx__date-picker__day-names) {
+		font-size: 0.7rem !important;
+	}
+
+	:global(.sx__calendar .sx__date-picker__week) {
+		font-size: 0.4rem !important;
+	}
+
+	:global(.sx__date-picker-popup) {
+		max-width: 300px !important;
+		max-height: 350px !important;
 	}
 
 	:global(.sx__calendar) {
