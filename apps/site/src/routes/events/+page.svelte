14971646a0f08478f3db3784621b6ae086f80045
aanyal <aanyalakhani01@gmail.com>
2026-04-11 17:35:29 -0700

    Configured the colors

--- apps/site/src/routes/events/+page.svelte
+++ apps/site/src/routes/events/+page.svelte
@@ -5,8 +5,9 @@
 	import { ScheduleXCalendar } from '@schedule-x/svelte';
 	import { createCalendar, createViewMonthGrid } from '@schedule-x/calendar';
 	import '@schedule-x/theme-default/dist/index.css';
-	import 'temporal-polyfill/global';
+
 	import { Temporal } from 'temporal-polyfill';
+	import 'temporal-polyfill/global';
 
 	let calendarApp;
 
@@ -49,9 +50,22 @@
 	.calendar-wrapper {
 		width: 100%;
 	}
+
 	:global(.sx-svelte-calendar-wrapper) {
 		height: 100vh;
+
+		--sx-color-background: #151515;
+		--sx-internal-color-text: #fff;
+
+		--sx-border: 1px solid #3d3d3d;
+
+		--sx-color-primary: #888888;
+
+		--sx-color-on-primary-container: #21005e;
+		--sx-color-surface-tint: #ff9db7;
+		--sx-color-on-background: #fff;
 	}
+
 	:global(.sx__calendar) {
 		@apply type-label; /* This works if using UnoCSS/Tailwind */
 	}
