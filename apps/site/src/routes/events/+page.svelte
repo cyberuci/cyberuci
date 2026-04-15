de495b0fffad1517f669c1cb091357b75f97d1b1
aanyal <aanyalakhani01@gmail.com>
2026-04-14 19:57:45 -0700

    Added side recent events

--- apps/site/src/routes/events/+page.svelte
+++ apps/site/src/routes/events/+page.svelte
@@ -18,8 +18,12 @@
 
 	let calendarApp = $state();
 
+	// let currentTime = Temporal.Now.zonedDateTimeISO();
+
+	let calendarEvents = $state([]);
+
 	onMount(() => {
-		const calendar_events = data.events.map((event, i) => {
+		calendarEvents = data.events.map((event, i) => {
 			const startStr = event.start.dateTime || `${event.start.date}T00:00:00Z`;
 			const endStr = event.end.dateTime || `${event.end.date}T23:59:59Z`;
 
@@ -28,11 +32,11 @@
 				title: event.summary,
 				start: Temporal.ZonedDateTime.from(`${startStr}[America/Los_Angeles]`),
 				end: Temporal.ZonedDateTime.from(`${endStr}[America/Los_Angeles]`),
-				calendarId: 'work'
+				calendarId: 'personal'
 			};
 		});
 
-		console.log(calendar_events);
+		console.log(calendarEvents);
 
 		calendarApp = createCalendar({
 			views: [createViewMonthGrid()],
@@ -54,10 +58,27 @@
 					}
 				}
 			},
-			events: calendar_events,
+			events: calendarEvents,
+			callbacks: {
+				onEventClick(event) {
+					console.log('Event clicked:', event);
+					if (event.url) window.open(event.url, '_blank');
+				}
+			},
 			timezone: Intl.DateTimeFormat().resolvedOptions().timeZone
 		});
 	});
+	let options: Intl.DateTimeFormatOptions = {
+		weekday: 'short',
+		year: 'numeric',
+		month: 'short',
+		day: 'numeric'
+	};
+
+	let timeOptions: Intl.DateTimeFormatOptions = {
+		hour: 'numeric',
+		minute: 'numeric'
+	};
 </script>
 
 <svelte:head>
@@ -67,10 +88,32 @@
 <main class="my-40 space-x">
 	<Title title="Events" />
 
-	<div class="calendar-wrapper">
-		{#if calendarApp}
-			<ScheduleXCalendar {calendarApp} />
-		{/if}
+	<div style="display: flex; flex-direction: row;">
+		<div style="width: 65%">
+			<div class="calendar-wrapper">
+				{#if calendarApp}
+					<ScheduleXCalendar {calendarApp} />
+				{/if}
+			</div>
+		</div>
+
+		<div style="width: 35%; padding-left: 40px;">
+			<p class="type-label" style="font-size: 20px">Upcoming Events</p>
+
+			{#each calendarEvents as event (event.id)}
+				<div
+					style="width: 100%; background-color: #3D3D3D; border-radius: 10px; padding: 1px; padding-left: 17px; padding-right: 17px; margin-bottom: 10px"
+				>
+					<!-- <p>{Temporal.PlainDateTime.compare(currentTime, event.start)}</p> -->
+					<p class="type-label">
+						{new Intl.DateTimeFormat(undefined, timeOptions).format(event.start.toPlainTime())}
+						{new Intl.DateTimeFormat('en-US', options).format(
+							new Date(event.start.toLocaleString())
+						)}: {event.title}
+					</p>
+				</div>
+			{/each}
+		</div>
 	</div>
 </main>
 
@@ -117,5 +160,10 @@
 
 	:global(.sx__calendar) {
 		@apply type-label; /* This works if using UnoCSS/Tailwind */
+	}
+
+	/* Optional: Add a browser tooltip for the full name */
+	:global(.sx__event) {
+		cursor: pointer;
 	}
 </style>
