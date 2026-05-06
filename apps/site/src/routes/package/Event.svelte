9a023b8afbdac744a473c372bd537b33036527a4
aanyal <aanyalakhani01@gmail.com>
2026-05-05 20:37:59 -0700

    Centers events text

--- apps/site/src/routes/package/Event.svelte
+++ apps/site/src/routes/package/Event.svelte
@@ -8,9 +8,11 @@
 	let { title, description, background }: Props = $props();
 </script>
 
-<div class={background ? 'rounded background-2 p-8' : ''}>
-	<h3 class="mb-3 type-heading-2">{title}</h3>
-	<p class="type-body-1 text-gray-11 line-height-relaxed dark:text-graydark-11">
-		{description}
-	</p>
+<div class={`${background ? 'rounded background-2 p-8' : ''} flex items-center`}>
+	<div>
+		<h3 class="mb-3 type-heading-2">{title}</h3>
+		<p class="type-body-1 text-gray-11 line-height-relaxed dark:text-graydark-11">
+			{description}
+		</p>
+	</div>
 </div>
