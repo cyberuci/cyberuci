14f8c2521264bde77a51a2d45afc07545f9cf84f
Akshay Rohatgi <52616034+Akshay-Rohatgi@users.noreply.github.com>
2026-09-18 13:02:16 -0700

    Revert local environment config change

--- apps/site/svelte.config.js
+++ apps/site/svelte.config.js
@@ -1,8 +1,5 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { fileURLToPath } from 'node:url';
-
-const monorepoRoot = fileURLToPath(new URL('../..', import.meta.url));
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,10 +8,10 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({}),
-		env: {
-			dir: monorepoRoot
-		}
+		adapter: adapter({})
+	},
+	env: {
+		dir: '../..' // Points back to the monorepo root
 	}
 };
 
